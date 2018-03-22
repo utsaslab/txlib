575266776a16b4e46954e2b3868068b7fa32b55b
Eric Lee <ericlee123@gmail.com>
2018-03-21 23:06:31 -0500

    cleaned up crash.c

--- tests/crash.c
+++ tests/crash.c
@@ -10,22 +10,20 @@
 
 #include "txnlib.h"
 
-char noise[50000];
+#define ATOM 50000
+
+char noise[ATOM];
 
 void spam_write()
 {
-        int times = 100000;
-        size_t size = 50000;
-
         int fd = open("out/spam.txt", O_CREAT | O_RDWR | O_EXCL, 0644);
         int id = begin_txn();
-        for (int i = 0; i < times; i++) {
+
+        while (1) {
                 if (id == -1)
                         id = begin_txn();
 
-                write(fd, noise, size);
-                if (i % 500 == 0)
-                        printf("%d\n", i);
+                write(fd, noise, ATOM);
 
                 int roll = rand() % 100;
                 if (roll < 5) {
@@ -33,18 +31,16 @@ void spam_write()
                         id = -1;
                 }
         }
-        end_txn(id);
-        close(fd);
 }
 
 int main()
 {
         srand(time(NULL));
 
-        int num_tests = 100;
+        int num_tests = 10000;
 
         int n = open("/dev/random", O_RDONLY);
-        read(n, noise, 50000);
+        read(n, noise, ATOM);
         close(n);
 
         for (int i = 0; i < num_tests; i++) {
@@ -53,33 +49,30 @@ int main()
                         spam_write();
                         return 0;
                 } else {
-                        int sec = rand() % (5 + 1);
-                        // int sec = 0;
+                        int sec = rand() % (0 + 1);
                         long nsec = rand() % (999999999 + 1);
                         struct timespec ts;
                         ts.tv_sec = sec;
                         ts.tv_nsec = nsec;
 
-                        printf(">>>>> %d days remain (%d s + %ld ns)\n", num_tests-i-1, sec, nsec);
-                        nanosleep(&ts, NULL);
+                        printf("--- test #%d (%d s + %09ld ns) ... ", i, sec, nsec);
 
+                        nanosleep(&ts, NULL);
                         kill(child, SIGKILL);
-                        printf("<<<<< killed\n");
+                        wait(NULL);
 
                         // verify valid state
                         int fd0 = open("out/spam.txt", O_CREAT | O_RDWR, 0644); // trigger recovery
                         struct stat st;
                         fstat(fd0, &st);
-                        if (st.st_size % 50000 != 0) {
-                                printf("ERROR: transactional guarantees not provided! %ld\n", st.st_size);
+                        if (st.st_size % ATOM != 0) {
+                                printf("\nERROR: transactional guarantees not provided! %ld\n", st.st_size);
                                 abort();
                         } else {
-                                printf("File in valid state! :)\n");
+                                printf("✓✓✓ File in valid state! ✓✓✓\n");
                         }
                         close(fd0);
                         remove("out/spam.txt");
-
-                        wait(NULL);
                 }
         }
 
