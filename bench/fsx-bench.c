6d31007374e75220ca91c71d736e462c987498e2
Eric Lee <ericlee123@gmail.com>
2018-04-26 00:26:01 -0500

    fixed some formatting

--- bench/fsx-bench.c
+++ bench/fsx-bench.c
@@ -9,22 +9,22 @@ int main()
 
         FILE *fp1 = popen("make clean && make && make fsx", "r");
         fgets(output, sizeof(output) - 1, fp1);
-        mem_writes = atoi(output);
+        mem_writes = atol(output);
         fgets(output, sizeof(output) - 1, fp1);
-        mem_truncs = atoi(output);
+        mem_truncs = atol(output);
         pclose(fp1);
 
         FILE *fp2 = popen("make clean && make && make fsx-txn", "r");
         fgets(output, sizeof(output) - 1, fp2);
-        txn_writes = atoi(output);
+        txn_writes = atol(output);
         fgets(output, sizeof(output) - 1, fp2);
-        txn_truncs = atoi(output);
+        txn_truncs = atol(output);
         pclose(fp2);
 
         printf("writes:\n");
-        printf("  mem -> %02lds %09ldns\n", mem_writes / 1000000000, mem_writes % 1000000000);
-        printf("  txn -> %02lds %09ldns (overhead: %4.2fx)\n", txn_writes / 1000000000, txn_writes % 1000000000, (double) txn_writes / mem_writes);
+        printf("  mem -> %2lds %9ldns\n", mem_writes / 1000000000, mem_writes % 1000000000);
+        printf("  txn -> %2lds %9ldns (overhead: %4.2fx)\n", txn_writes / 1000000000, txn_writes % 1000000000, (double) txn_writes / mem_writes);
         printf("truncates:\n");
-        printf("  mem -> %02lds %09ldns\n", mem_truncs / 1000000000, mem_truncs % 1000000000);
-        printf("  txn -> %02lds %09ldns (overhead: %4.2fx)\n", txn_truncs / 1000000000, txn_truncs % 1000000000, (double) txn_truncs / mem_truncs);
+        printf("  mem -> %2lds %9ldns\n", mem_truncs / 1000000000, mem_truncs % 1000000000);
+        printf("  txn -> %2lds %9ldns (overhead: %4.2fx)\n", txn_truncs / 1000000000, txn_truncs % 1000000000, (double) txn_truncs / mem_truncs);
 }
